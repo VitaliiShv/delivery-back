f0f838dcf7169534e03587ff4f93fecfe7da1b02
VitaliiShv <vitalyashved@gmail.com>
2023-06-01 19:15:39 +0300

    configure enviroment variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const productsRouter = require("./routes/api/products");
 const restaurantsRouter = require("./routes/api/restaurants");

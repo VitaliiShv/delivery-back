34ed2c43cd3fe3f1fa875ba037899e4b470b10a7
VitaliiShv <vitalyashved@gmail.com>
2023-06-01 19:02:23 +0300

    add DB_HOST to server settings

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://vitalii:qSuKcqLo6W3tF6cU@cluster0.xgy3gur.mongodb.net/delivery?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => app.listen(3030))
   .catch((error) => console.log(error.message));
-
-//qSuKcqLo6W3tF6cU
